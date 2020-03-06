b9a0544d7fa5b542bcd37f66a7921f6385f2c8af
arshdeep1105 <arshdeep1105sbnr@gmail.com>
2020-03-05 20:22:21 -0500

    Add project files.

---
+++ PhoneApplication/Controllers/PhoneController.cs
@@ -0,0 +1,37 @@
+﻿using PhoneApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhoneApplication.Controllers
+{
+    public class PhoneController : Controller
+    {
+        [NonAction]
+        public List<Phone> GetPhoneList()
+        {
+            return new List<Phone>
+            {
+                new Phone
+                {
+                    PhoneID = 5,
+                    PhoneName = "S20",
+                    MSRP = 1000,
+                    ScreenSize = 13.2,
+                    DateReleased = DateTime.Now,
+                    Manufacturer = "Samsung"
+                },
+            };
+        }
+        // GET: Phone
+        public ActionResult Index()
+        {
+
+            var Phone = from p in GetPhoneList() orderby p.PhoneID select p;
+            return View(Phone);
+        }
+
+    }
+}
